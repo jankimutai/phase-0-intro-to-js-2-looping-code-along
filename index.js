54487e400633689ff4e94e348b0eea13407e4b76
Kimutai <jankimsingoei@gmail.com>
2023-07-16 18:57:51 +0300

    done!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let messages=["Guadalupe", "Ollie", "Aki"];
+let response=[];
+function writeCards(){
+    for (let mes=0; mes < messages.length; mes++){
+        let thankYou = "Thank you"
+        let fotThe = "for the wonderful surprise gift!"
+        response[mes]= (`${thankYou}, ${messages[mes]}, ${fotThe}`)
+        debugger;
+    }
+    return response
+}
+function countDown(){
+    let count=10;
+    while(count>=0){
+        console.log (count--);
+
+    }
+    
+}
